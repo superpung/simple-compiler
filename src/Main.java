bd838cc924cf669461764c91531917b1aecf0222
SUPER <superpung.lee@gmail.com>
2021-10-26 21:02:44 +0800

    :white_check_mark: add test

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+/**
+ * @author super
+ */
+public class Main {
+    public static void main(String[] argc) {
+        LexicalAnalysis lexicalAnalysis = new LexicalAnalysis("./test/test1.c");
+        lexicalAnalysis.printResult();
+    }
 }
