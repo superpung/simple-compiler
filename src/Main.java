f9cfaab3bf6b2b651e4a5c23e7fc9a5f4704dbe5
SUPER <superpung.lee@gmail.com>
2021-11-17 21:27:32 +0800

    :white_check_mark: pass test

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@
  */
 public class Main {
     public static void main(String[] argc) {
-        LexicalAnalysis lexicalAnalysis = new LexicalAnalysis("./test/test5.c");
-//        lexicalAnalysis.printResult();
-        SyntaxAnalysis syntaxAnalysis = new SyntaxAnalysis("./src/syntax.txt");
+        LexicalAnalysis lexicalAnalysis = new LexicalAnalysis("./test/test3.c");
+        lexicalAnalysis.printResult();
+        SyntaxAnalysis syntaxAnalysis = new SyntaxAnalysis("./src/syntax.txt", lexicalAnalysis.getTokensResult());
 //        syntaxAnalysis.printFirst();
-        syntaxAnalysis.printFollow();
+//        syntaxAnalysis.printFollow();
+        syntaxAnalysis.printTable();
+        syntaxAnalysis.printPredict("S");
     }
 }
