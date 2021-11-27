a66f22dcbe3c4d649bf83eb8991f531c8764181f
SUPER <superpung.lee@gmail.com>
2021-11-27 11:13:32 +0800

    :white_check_mark: add test

--- test/test1.c
+++ test/test1.c
@@ -1,6 +1,20 @@
-void main(){
-    int i = 2;
-    while(i <= 4){
-        i = i+1;
+_(int while_, int for_)
+{
+  int if_ = 1;
+
+  if(for_ == 0)
+  {
+    return 1;
+  }
+
+  while(for_ != 0){
+    if (for_ % 1)
+    {
+      if_ *= while_;
     }
+    for_ = for_ / 2;
+    while_ *= while_;
+  }
+
+  return if_;
 }
